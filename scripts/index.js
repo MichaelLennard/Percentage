9db83ec1664352b33ea508580b04b9bd25b56e41
MichaelLennard <bullet-00@hotmail.com>
2017-10-22 19:34:01 +0200

    first try with jquery

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,8 @@ class My {
   intIndicators() {
     $(".indicator").map((index, $indicator) => {
       new Indicator($indicator);
+
+      
     });
   }
 }
@@ -12,6 +14,28 @@ class My {
 class Indicator {
   constructor($indicator) {
     console.log($indicator);
+  //   const display = $('.line');
+  //   let currentValue = parseInt(display.text());
+    const zero = 0;
+  //   const diff =  zero - currentValue;
+  //   var step = ( 0 < diff ? 1 : -1 ); 
+
+  //   for (var i = 0; i < Math.abs(diff); ++i) {
+  //     setTimeout(function() {
+  //         currentValue += step
+  //         display.text(currentValue);
+  //     }, 100 * i)   
+  // } 
+
+  $(".text").click(function(){
+    let currentInfo = parseInt($('.info').text());
+    console.log(currentInfo);   
+    })
+
+
+
+
+
   }
 }
 
